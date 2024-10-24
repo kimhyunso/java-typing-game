0a1a9112f1910ed2c6fc110d66d4f98eabc8a2ae
kimhyunso <gustn9515@gmail.com>
2024-10-25 01:05:06 +0900

    feat: cli 테스트 gameViewTest 클래스 기능 구현

--- test/view/GameViewTest.java
+++ test/view/GameViewTest.java
@@ -2,20 +2,27 @@ package test.view;
 
 import java.util.Scanner;
 
+import test.view.settings.ModeSettingTest;
+import test.view.settings.ModeTest;
+
 public class GameViewTest {
+
+    private ModeTest modeTest;
 
     public GameViewTest() {
         Scanner input = new Scanner(System.in);
-        int mode = 0;
+        String mode = null;
 
-        while(mode != 5) {
+        while(!mode.equals("5")) {
             System.out.println("1. 싱글모드");
             System.out.println("2. 멀티모드");
             System.out.println("3. 설정");
             System.out.println("4. 명예의전당");
             System.out.println("5. 끝내기");
 
-            mode = input.nextInt();
+            mode = input.next();
+            for (ModeSettingTest modeSetting : ModeSettingTest.values()) {
+            }
 
         }       
     }
