787ba77c421778d06acf0258782e4f246b35c564
kimhyunso <gustn9515@gmail.com>
2024-10-24 15:51:14 +0900

    fix: 좀비 위치값 개선

---
+++ view/GameView.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class GameView extends JFrame {
+
+    private int defaultWidth = 800;
+    private int defaultHeight = 800;
+
+
+    public GameView(int width, int height) {
+        setTitle("Typing Game");
+        setSize(defaultWidth, defaultHeight);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+}
