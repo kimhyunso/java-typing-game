fa717363fc1c5736dbebfd321f678dc8a105c239
kimhyunso <gustn9515@gmail.com>
2024-10-25 15:55:34 +0900

    feat: [cli] 단어 모음 클래스 추가

--- src/test/java/org/example/view/components/MultiButtonTest.java
+++ src/test/java/org/example/view/components/MultiButtonTest.java
@@ -1,11 +1,41 @@
 package org.example.view.components;
 
+import org.example.domain.Archive;
 import org.example.view.settings.ModeTest;
+
+import java.util.Scanner;
 
 public class MultiButtonTest implements ModeTest {
 
+    private int score;
+
     @Override
     public void execute() {
-        System.out.println("멀티모드 플레이중...");
+        Scanner input = new Scanner(System.in);
+        System.out.println("==== 싱글모드 게임 ====");
+        int wordCount = 0;
+
+        while (true) {
+            if (wordCount >= 200) {
+                wordCount = 0;
+            }
+            String targetWord = Archive.WORDS.get(wordCount++);
+            System.out.println("단어: " + targetWord);
+            System.out.println("1. 텍스트 입력하기");
+            System.out.println("6. 메인 메뉴로 가기");
+            String mode = input.next();
+
+            if (!mode.equals("1")) {
+                break;
+            }
+
+            System.out.print("입력: ");
+            String word = input.next();
+            if (targetWord.equals(word)) {
+                score += 10;
+            }
+
+            System.out.println("점수 현황: " + score);
+        }
     }
 }
