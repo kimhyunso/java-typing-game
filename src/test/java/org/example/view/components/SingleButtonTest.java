4db6842dc301ec2fc6d79ad0680e8f94c1c1cbde
kimhyunso <gustn9515@gmail.com>
2024-10-25 15:17:49 +0900

    feat: gradle 프로젝트로 변경

--- src/test/java/org/example/view/components/SingleButtonTest.java
+++ src/test/java/org/example/view/components/SingleButtonTest.java
@@ -3,6 +3,7 @@ package org.example.view.components;
 
 import java.util.Scanner;
 
+import org.example.RedisHandler;
 import org.example.view.settings.ModeTest;
 
 public class SingleButtonTest implements ModeTest {
@@ -11,33 +12,35 @@ public class SingleButtonTest implements ModeTest {
     private String words = "강아지";
     // TODO: 스코어 저장
     private int score;
+    private RedisHandler handler;
 
     @Override
     public void execute() {
+        handler = new RedisHandler();
         Scanner input = new Scanner(System.in);
         
         System.out.println("==== 싱글모드 게임 ====");
 
-        // while (true) {
-        //     System.out.println("단어: " + words);
-        //     System.out.println("1. 텍스트 입력하기");
-        //     System.out.println("5. 끝내기");
-        //     System.out.println("6. 메인 메뉴로 가기");
-        //     String mode = input.next();
+         while (true) {
+             System.out.println("단어: " + words);
+             System.out.println("1. 텍스트 입력하기");
+             System.out.println("5. 끝내기");
+             System.out.println("6. 메인 메뉴로 가기");
+             String mode = input.next();
 
-        //     if (!mode.equals("1")) {
-        //         break;
-        //     }
+             if (!mode.equals("1")) {
+                 break;
+             }
 
-        //     String word = input.next();
-        //     if (words.equals(word)) {
-        //         score += 10;
-        //     }
-        //     System.out.println("단어: " + words);
-        //     words = "메추리알";
-        // }
-        // System.out.println("점수 현황: " + score);
-        // input.close();
+             String word = input.next();
+             if (words.equals(word)) {
+                 score += 10;
+             }
+             System.out.println("단어: " + words);
+             words = "메추리알";
+         }
+         System.out.println("점수 현황: " + score);
+         input.close();
     }
 
 }
