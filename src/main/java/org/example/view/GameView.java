f8c0e0cbf65280a589cca941dfbe4c8aca42ca36
kimhyunso <gustn9515@gmail.com>
2024-10-28 21:27:43 +0900

    fix: GameView 주석 해제

--- src/main/java/org/example/view/GameView.java
+++ src/main/java/org/example/view/GameView.java
@@ -1,5 +1,9 @@
 package org.example.view;
 
+import org.example.view.components.MainMenuPanel;
+import org.example.view.settings.Display;
+import org.example.view.settings.Version;
+
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -10,20 +14,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GameView extends JFrame {
-    // private static final int FARME_WIDTH = Display.WIDTH.value();
-    // private static final int FARME_HEIGHT = Display.HEIGHT.value();    
-    // private MainMenuPanel mainMenuPanel = new MainMenuPanel();
+     private static final int FARME_WIDTH = Display.WIDTH.value();
+     private static final int FARME_HEIGHT = Display.HEIGHT.value();
+     private MainMenuPanel mainMenuPanel = new MainMenuPanel();
     private JPanel cards;
 
     public GameView() {
-        // setTitle("Typing Game " + Version.VERSION.value());
-        // setSize(FARME_WIDTH, FARME_HEIGHT);
+        setTitle("Typing Game " + Version.VERSION.value());
+        setSize(FARME_WIDTH, FARME_HEIGHT);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		// setLocation((screenSize.width - FARME_WIDTH) / 2, (screenSize.height - FARME_HEIGHT) / 2);
-        // setLayout(new FlowLayout(FlowLayout.CENTER, 0, (FARME_HEIGHT -  mainMenuPanel.getPreferredSize().height) / 2));
+		setLocation((screenSize.width - FARME_WIDTH) / 2, (screenSize.height - FARME_HEIGHT) / 2);
+        setLayout(new FlowLayout(FlowLayout.CENTER, 0, (FARME_HEIGHT -  mainMenuPanel.getPreferredSize().height) / 2));
 
         cards = new JPanel(new CardLayout());
-        // cards.add(mainMenuPanel, "mainMenu");
+        cards.add(mainMenuPanel, "mainMenu");
         cards.revalidate();
 
         add(cards);
