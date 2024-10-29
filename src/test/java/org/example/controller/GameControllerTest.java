8bf97c204ca93fbab2524e75dc59aa40d65afa94
kimhyunso <gustn9515@gmail.com>
2024-10-29 17:16:42 +0900

    feat: GameControllerTest 클래스 생성

---
+++ src/test/java/org/example/controller/GameControllerTest.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.example.domain.GameModelTest;
+
+public class GameControllerTest {
+    private GameModelTest gameModel;
+
+    public GameControllerTest(GameModelTest gameModel) {
+        this.gameModel = gameModel;
+    }
+
+}
--- src/test/java/org/example/domain/GameModelTest.java
+++ src/test/java/org/example/domain/GameModelTest.java
@@ -1,6 +1,16 @@
 package org.example.domain;
 
+import org.example.controller.GameControllerTest;
+import org.example.domain.Image.AttackImage;
+import org.example.domain.Image.ImageFactory;
+import org.example.domain.Image.MoveImage;
+import org.example.domain.Image.RunImage;
+
 public class GameModelTest {
+    private AttackImage attackImage;
+    private MoveImage moveImage;
+    private RunImage runImage;
+
 
 
 
