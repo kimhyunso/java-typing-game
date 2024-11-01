d5a7500ad9e9b257b9ea2284ad592850fba1a9aa
kimhyunso <gustn9515@gmail.com>
2024-11-01 17:37:04 +0900

    main 수정

--- src/main/java/org/example/view/frame/ChooseFrame.java
+++ src/main/java/org/example/view/frame/ChooseFrame.java
@@ -16,17 +16,13 @@ import javax.swing.JFrame;
 public class ChooseFrame extends JFrame implements GameView {
      private int frameWidth = Display.WIDTH.value();
      private int frameHeight = Display.HEIGHT.value();
-     private MainMenuPanel mainMenuPanel;
 
-    public ChooseFrame(FrameController frameController) {
-        mainMenuPanel = new MainMenuPanel(frameController);
+    public ChooseFrame() {
         setTitle("Typing Game " + Version.VERSION.value());
         setSize(frameWidth, frameHeight);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation((screenSize.width - frameWidth) / 2, (screenSize.height - frameHeight) / 2);
-        setLayout(new FlowLayout(FlowLayout.CENTER, 0, (frameHeight -  mainMenuPanel.getPreferredSize().height) / 2));
 
-        add(mainMenuPanel);
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
--- src/main/java/org/example/view/frame/panel/MainMenuPanel.java
+++ src/main/java/org/example/view/frame/panel/MainMenuPanel.java
@@ -6,7 +6,6 @@ import org.example.view.components.*;
 import javax.swing.*;
 
 public class MainMenuPanel extends JPanel {
-
     public MainMenuPanel(FrameController frameController) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(new SingleModeButton(frameController));
