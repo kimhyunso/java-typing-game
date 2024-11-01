e8d150fe20064baa379719cfaf8f2ca3a4f92c73
kimhyunso <gustn9515@gmail.com>
2024-11-01 15:49:45 +0900

    fix: workflows 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.view.GameView;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        GameView gameView;
+        FrameController frameController = new FrameController();
     }
 }
