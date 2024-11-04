92455e66c629032a0af35b1b1a38c86e7a2149bd
kimhyunso <gustn9515@gmail.com>
2024-11-04 11:16:07 +0900

    fix: 캐릭터 이미지 보여주기 기능 수정

--- src/main/java/org/example/view/frame/CharacterChooseFrame.java
+++ src/main/java/org/example/view/frame/CharacterChooseFrame.java
@@ -1,6 +1,8 @@
 package org.example.view.frame;
 
 import org.example.view.frame.panel.DavidthompsonPanel;
+import org.example.view.frame.panel.JamescarterPanel;
+import org.example.view.frame.panel.JohnmillerPanel;
 import org.example.view.settings.Display;
 import org.example.view.settings.Version;
 
@@ -17,10 +19,18 @@ public class CharacterChooseFrame extends JFrame {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation((screenSize.width - frameWidth) / 2, (screenSize.height - frameHeight) / 2);
 
+        setLayout(new GridLayout(1, 3));
+
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
 
         DavidthompsonPanel davidthompsonPanel = new DavidthompsonPanel();
         add(davidthompsonPanel);
+
+        JohnmillerPanel johnmillerPanel = new JohnmillerPanel();
+        add(johnmillerPanel);
+
+        JamescarterPanel jamescarterPanel = new JamescarterPanel();
+        add(jamescarterPanel);
     }
 }
--- src/main/java/org/example/view/frame/panel/JamescarterPanel.java
+++ src/main/java/org/example/view/frame/panel/JamescarterPanel.java
@@ -3,6 +3,7 @@ package org.example.view.frame.panel;
 import org.example.model.image.CharacterImageFactory;
 import org.example.model.player.UnitType;
 import org.example.view.components.CharacterChooseButton;
+import org.example.view.settings.ButtonSize;
 
 import javax.swing.*;
 import java.awt.*;
@@ -23,7 +24,7 @@ public class JamescarterPanel extends JPanel {
                 imageFactory.createImage("soldier_jamescarter", UnitType.SOLDIER, "idle");
 
         for (BufferedImage image : davidthompsonIdleImage) {
-            g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(),this);
+            g.drawImage(image, 0, ButtonSize.HEIGHT.value(), image.getWidth(), image.getHeight(),this);
         }
     }
 }
